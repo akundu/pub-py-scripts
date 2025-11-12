54db24f299430fb271e77d665499e06450be6b67
Anirban Kundu <anirbankundu@gmail.com>
2025-11-12 08:46:50 -0800

    cleanups & updating values

--- stocks/run_scripts/covered_call_generation.sh
+++ stocks/run_scripts/covered_call_generation.sh
@@ -2,41 +2,69 @@
 
 STORE_DIR="/Users/akundu/programs/http-proxy/static/"
 OUTPUT_DIR="stocks_to_buy"
+DOWNLOAD_LOC="~/Downloads/results.csv"
+QUERY_LOC="questdb://stock_user:stock_password@localhost:8812/stock_data"
 
-TYPE=types
-TYPE_input=all
+TYPE="types"
+TYPE_input="all"
 
-MAX_WORKERS=10
+MAX_WORKERS=7
 BATCH_SIZE=300
 
 MAX_DAYS=`days=$((6 - $(date +%u)));[ $days -le 0 ] && days=$((days + 7)); echo $days` #count the number of days till the next saturday
-MAX_DAYS=30
+MAX_DAYS=21
 TIME_TO_USE=`TZ='America/New_York' date -v-30M '+%Y-%m-%d %H:%M:%S'`
 
-POSITION_SIZE=100000
+POSITION_SIZE=100000 #amt to invest
 
-QUERY_LOC="questdb://stock_user:stock_password@localhost:8812/stock_data"
 SORT="DAY_PREM"
 TOP_N=1
 
-MIN_PE=10
-MIN_VOL=100
-MIN_LONG_PREMIUM=1.5
-MIN_PREMIUM=1
+MIN_PE=1
+MIN_VOL=50
+MIN_LONG_PREMIUM=0.5
+MIN_PREMIUM=0.25
 MIN_NET_PREMIUM=1000
-current_price=1.005
+CURR_PRIC_MULT=1
 
-while true; 
-do 
+
+#spread info
+spread_strike_tolerance=5
+spread_long_days=90
+spread_long_min_days=60
+spread_long_days_tolerance=10
+
+while true;
+do
     date
 
     start=$(date +%s)
-    rc = $(python scripts/options_analyzer.py --top-n 1 --stats --max-days $MAX_DAYS --batch-size $BATCH_SIZE --sort net_daily_premium  --position-size $POSITION_SIZE --$TYPE $TYPE_input --spread --spread-strike-tolerance 5.0 --spread-long-days 90 --spread-long-min-days 60 --spread-long-days-tolerance 10 --db-conn $QUERY_LOC --max-workers $MAX_WORKERS  --output ~/Downloads/results.csv  --filter "strike_price > current_price*$curr_pric_mult" --filter "volume > $MIN_VOL" --filter "long_option_premium > $MIN_LONG_PREMIUM" --filter "option_premium > $MIN_PREMIUM" --filter "premium_diff > 0" --filter "pe_ratio > $MIN_PE" --filter "net_premium > $MIN_NET_PREMIUM" --sort $SORT)
-    echo "Elapsed: $(($(date +%s) - start)) seconds"
+    command="python scripts/options_analyzer.py --top-n $TOP_N --stats --max-days $MAX_DAYS \
+      --batch-size $BATCH_SIZE --max-workers $MAX_WORKERS  \
+      --sort net_daily_premium  \
+      --position-size $POSITION_SIZE \
+      --$TYPE $TYPE_input \
+      --spread --spread-strike-tolerance $spread_strike_tolerance --spread-long-days $spread_long_days --spread-long-min-days $spread_long_min_days --spread-long-days-tolerance $spread_long_days_tolerance \
+      --db-conn $QUERY_LOC \
+      --output $DOWNLOAD_LOC \
+      --filter \"current_price*$CURR_PRIC_MULT < strike_price\" \
+      --filter \"volume > $MIN_VOL\" \
+      --filter \"long_option_premium > $MIN_LONG_PREMIUM\" \
+      --filter \"option_premium > $MIN_PREMIUM\" \
+      --filter \"premium_diff > 0\" \
+      --filter \"net_premium > $MIN_NET_PREMIUM\" \
+      --min-write-timestamp \"$TIME_TO_USE\" \
+      --sort $SORT" #--filter "pe_ratio > $MIN_PE"
 
-    #save to a location
+    echo "running $command"
+    eval $command
+    rc=$?
+    echo "Elapsed: $(($(date +%s) - start)) seconds with result = $rc"
+
     if [ $rc -eq 0 ]; then
-        python scripts/evaluate_covered_calls.py --file ~/Downloads/results.csv --html --output-dir /tmp/$OUTPUT_DIR && rm -Rf $STORE_DIR/$OUTPUT_DIR && mv /tmp/$OUTPUT_DIR $STORE_DIR
+      python scripts/evaluate_covered_calls.py --file $DOWNLOAD_LOC --html --output-dir /tmp/$OUTPUT_DIR && \
+        rm -Rf $STORE_DIR/$OUTPUT_DIR && \
+        mv /tmp/$OUTPUT_DIR $STORE_DIR
     fi
 
     sleep 10
