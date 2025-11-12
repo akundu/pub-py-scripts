c7fb27c2330654d5c1e598b4db8e61f9e13003db
Anirban Kundu <anirbankundu@gmail.com>
2025-11-11 20:59:59 -0800

    updated parameters

--- stocks/run_scripts/covered_call_generation.sh
+++ stocks/run_scripts/covered_call_generation.sh
@@ -10,7 +10,7 @@ MAX_WORKERS=8
 BATCH_SIZE=300
 
 days_1=`days=$((6 - $(date +%u)));[ $days -le 0 ] && days=$((days + 7)); echo $days` #count the number of days till the next saturday
-days_1=30
+days_1=14
 TIME_TO_USE=`TZ='America/New_York' date -v-30M '+%Y-%m-%d %H:%M:%S'`
 
 POSITION_SIZE=100000
@@ -19,12 +19,17 @@ QUERY_LOC="questdb://stock_user:stock_password@localhost:8812/stock_data"
 SORT="DAY_PREM"
 TOP_N=1
 
+MIN_PE=5
+MIN_VOL=100
+MIN_LONG_PREMIUM=0.5
+MIN_PREMIUM=0.25
+
 while true; 
 do 
     date
 
     start=$(date +%s)
-    python scripts/options_analyzer.py --top-n 1 --stats --max-days $days_1 --batch-size $BATCH_SIZE --sort net_daily_premium  --position-size $POSITION_SIZE --$TYPE $TYPE_input --spread --spread-strike-tolerance 5.0 --spread-long-days 120 --spread-long-min-days 90 --spread-long-days-tolerance 30  --db-conn $QUERY_LOC --max-workers $MAX_WORKERS  --output ~/Downloads/results.csv  --filter "strike_price > current_price" --filter "volume > 50" --filter "long_option_premium > 0.25" --filter "option_premium > .25" --filter "delta<0.35" --filter "premium_diff > 0" --sort $SORT
+    python scripts/options_analyzer.py --top-n 1 --stats --max-days $days_1 --batch-size $BATCH_SIZE --sort net_daily_premium  --position-size $POSITION_SIZE --$TYPE $TYPE_input --spread --spread-strike-tolerance 5.0 --spread-long-days 90 --spread-long-min-days 60 --spread-long-days-tolerance 10 --db-conn $QUERY_LOC --max-workers $MAX_WORKERS  --output ~/Downloads/results.csv  --filter "strike_price > current_price" --filter "volume > $MIN_VOL" --filter "long_option_premium > $MIN_LONG_PREMIUM" --filter "option_premium > $MIN_PREMIUM" --filter "delta<0.35" --filter "premium_diff > 0" --filter "pe_ratio > $MIN_PE" --sort $SORT
     echo "Elapsed: $(($(date +%s) - start)) seconds"
 
     #save to a location
